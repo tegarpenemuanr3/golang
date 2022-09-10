59771b961e8ac547fdee072a2a6439ccc564c2a5
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2022-09-10 22:40:39 +0700

    operator relasional

---
+++ src/operator_relasional.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// * == sama dengan
+	// * != tidak sama dengan
+	// * > lebih besar
+	// * < lebih kecil
+	// * >= lebih besar sama dengan
+	// * <= lebih kecil sama dengan
+
+	fmt.Printf("true atau false: %t \n", 1 == 1)
+}
+
+// ! Running Terminal - go run .\src\operator_relasional.go
