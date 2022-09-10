0df83bf4a4b70999b5545e5562ca5739dad21afa
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2022-09-10 22:35:34 +0700

    Operator Aritmatika

---
+++ src/operator_aritmatika.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// * + Penjumlahan 
+	// * - Pengurangan
+	// * * Perkalian
+	// * / Pembagian
+	// * % Modulus
+	// * ++ increment operator
+	// * -- decrement operator
+
+	var angka1 int = 10
+	var angka2 int = 5
+
+	hasil := angka1 + angka2
+	fmt.Println(hasil)
+
+	nomor := 10
+	nomor++
+	fmt.Println(nomor)
+}
+
+// ! Running Terminal - go run .\src\operator_aritmatika.go
