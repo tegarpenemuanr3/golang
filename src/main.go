9063ec6912795ab8014af05c45cd76adc0f5899a
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2022-09-10 22:14:10 +0700

    fmt Printf dan Sprintf

--- src/main.go
+++ src/main.go
@@ -10,6 +10,7 @@ func main() {
     fmt.Println("Hello World")
 
     pariabel()
+    sptf()
 }
 
 func pariabel() {
@@ -37,5 +38,52 @@ func pariabel() {
     hola := "Hai dalam bahasa Spanyol"
     fmt.Println(hola)
 }
+
+func sptf() {
+    var number float32 = 10.0
+    fmt.Printf("\nTipe data: %T dan Value: %v \n",number,number)
+
+    //tampung dalam variabel
+    var myString string = fmt.Sprintf("Tipe data: %T dan Value: %v",number,number)
+    fmt.Println(myString)
+
+    //menampilkan true dan false
+    fmt.Printf("TRUE atau FALSE: %t \n", 1 < 2)
+
+    //menampilkan binary (base 2) dari sebuah angka
+    fmt.Printf("Binary: %b \n",10)
+
+    //menampilkan oktal
+    fmt.Printf("Oktal: %o \n", 10)
+
+    //menampilkan desimal
+    fmt.Printf("Desimal: %d \n", 10)
+
+    //menampilkan hexadecimal
+    fmt.Printf("Hexa: %x \n", 10)
+
+    //menampilkan angka decimal dan dibulatkan
+    fmt.Printf("Hasil: %f \n",2.1029322222222222)
+
+    //menampilkan angka decimal secara menyeluruh
+    fmt.Printf("Hasil: %g \n",2.1029322222222222)
+
+    //menampilkan string default
+    fmt.Printf("Bahasa: %s \n","Golang")
+
+    //menampilkan string dalam tanda kutip
+    fmt.Printf("Bahasa: %q \n", "Golang")
+
+    //Panjang ke kiri
+    fmt.Printf("Hello: %12s Tegar \n", "Penemuan")
+
+    //Panjang ke kanan
+    fmt.Printf("Hello: %-12s Tegar \n", "Penemuan")
+
+    //panjang default, presisi 2
+    fmt.Printf("Angka %.2f \n", 10.11231)
+}
+
+
 
 // ! Running Terminal - go run .\src\main.go
